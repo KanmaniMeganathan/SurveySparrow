9496aa701cb1087ff1d5dfc72dece180ee9b0e67
KanmaniMeganathan <kanmanimcareer@gmail.com>
2024-09-16 02:01:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,9 @@ import pandas as pd
 
 import subprocess
 
-# Flask API URL
 api_url = "http://127.0.0.1:5000/api/commits"
 
-# Fetch data from Flask API
+
 @st.cache_data
 def fetch_data():
     response = requests.get(api_url)
@@ -20,10 +19,10 @@ def start_flask():
     process = subprocess.Popen(['python', 'app.py'])
     return process
 
-# Start Flask server
+
 flask_process = start_flask()
     
-# Display data in the Streamlit dashboard
+
 st.title('GitHub - scikit-learn Commits Dashboard')
 
 data = fetch_data()
